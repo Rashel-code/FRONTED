58cd22159add8bf81a04cfc98380cb05d509ca30
Rashel-code <82184216+Rashel-code@users.noreply.github.com>
2022-10-04 16:59:19 -0500

    Add files via upload

---
+++ ordenpais.js
@@ -0,0 +1,16 @@
+const config = {
+    type: 'scatter',
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Scatter Chart'
+        }
+      }
+    },
+  };
